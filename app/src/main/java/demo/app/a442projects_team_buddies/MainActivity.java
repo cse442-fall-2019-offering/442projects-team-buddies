7891c7ec123243af5f7d95f4e5db32903949e675
easXin <codingericwang@gmail.com>
2019-09-18 12:06:06 -0400

    setup development env for android project

---
+++ app/src/main/java/demo/app/a442projects_team_buddies/MainActivity.java
@@ -0,0 +1,14 @@
+package demo.app.a442projects_team_buddies;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
