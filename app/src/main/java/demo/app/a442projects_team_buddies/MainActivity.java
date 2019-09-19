76604c2c6148905d3a4f8996e9b830c49e56e04d
easXin <codingericwang@gmail.com>
2019-09-18 23:11:47 -0400

    complete version of the side nevigation

--- app/src/main/java/demo/app/a442projects_team_buddies/MainActivity.java
+++ app/src/main/java/demo/app/a442projects_team_buddies/MainActivity.java
@@ -1,14 +1,36 @@
 package demo.app.a442projects_team_buddies;
 
+import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.drawerlayout.widget.DrawerLayout;
 
 import android.os.Bundle;
+import android.view.MenuItem;
 
 public class MainActivity extends AppCompatActivity {
+    private DrawerLayout drawLayout;
+    private ActionBarDrawerToggle toggle;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        initWidgets();
+        toggle = new ActionBarDrawerToggle(this,drawLayout,R.string.open,R.string.close);
+        drawLayout.addDrawerListener(toggle);
+        toggle.syncState();
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+    }
+
+    public void initWidgets(){
+        drawLayout = findViewById(R.id.drawerLayout);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item){
+        if(toggle.onOptionsItemSelected(item)) {
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
     }
 }
