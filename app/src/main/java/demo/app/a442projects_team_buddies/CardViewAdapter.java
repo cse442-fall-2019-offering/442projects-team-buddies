47bbaa06dc3ff7b78604f20a5ad91a0d8bafb3c4
Sultan <sultanol@buffalo.edu>
2019-10-20 16:56:33 -0400

    cardview courses

---
+++ app/src/main/java/demo/app/a442projects_team_buddies/CardViewAdapter.java
@@ -0,0 +1,6 @@
+package demo.app.a442projects_team_buddies;
+
+import androidx.appcompat.app.AlertController;
+
+public class CardViewAdapter extends AlertController.RecycleListView {
+}
---
+++ app/src/main/java/demo/app/a442projects_team_buddies/NewCourseViewActivity.java
@@ -0,0 +1,14 @@
+package demo.app.a442projects_team_buddies;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class NewCourseViewActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_new_course_view);
+    }
+}
