4266caaa5c7461c749dc6d928d9c24c454f58c8d
Sultan <sultanol@buffalo.edu>
2019-11-05 01:10:14 -0500

    using actual users

--- app/src/main/java/demo/app/a442projects_team_buddies/Messages.java
+++ app/src/main/java/demo/app/a442projects_team_buddies/Messages.java
@@ -5,34 +5,77 @@ import androidx.appcompat.widget.Toolbar;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.ListAdapter;
 import android.widget.ListView;
+import com.parse.FindCallback;
+import com.parse.ParseException;
+import com.parse.ParseObject;
+import com.parse.ParseQuery;
+import com.parse.ParseUser;
+
+import java.util.List;
 
 public class Messages extends AppCompatActivity {
     Toolbar toolbar;
+    String friendList[];
+    Integer images[];
+
+
+
+    public void addFriends(int size, List<ParseObject> friends){
+
+        friendList = new String[size];
+        images= new Integer[size];
+        int i=0;
+        for(ParseObject person: friends){
+            String userFriend= person.toString();
+            friendList[i]=userFriend;
+            images[i]= R.drawable.photo_circle;  // needs to change for each user picture
+
+            i++;
+        }
+
+    }
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_messages);
 
+
+        ParseQuery<ParseObject> userFriends= ParseQuery.getQuery("User_Friends");
+        userFriends.whereEqualTo("user_name", ParseUser.getCurrentUser().getUsername());
+        userFriends.findInBackground(new FindCallback<ParseObject>() {
+            public void done(List<ParseObject> friendList, ParseException e) {
+                if (e == null) {
+                        addFriends(friendList.size(),friendList);
+                }
+            }
+        });
+
+
+
+
+
+
+
+
+
         toolbar = findViewById(R.id.MessageBar);
         //setSupportActionBar(toolbar);
        // getSupportActionBar().setTitle("Messages");
         toolbar.setLogo(R.drawable.ic_arrow_back_black_24dp);  //go back button
-        Integer images[] = new Integer[10];
-        String userNames[]= new String[10];
-        for(int i =0 ; i< 10 ; i++){
-            images[i]= R.drawable.photo_circle;
-            userNames[i] = "user"+(i+1);
-            }
-      //replace with for each user in match database
+
+
 
         ListView lv =(ListView)findViewById(R.id.messagesList);
 
-        MyListAdapter mlAdapter = new MyListAdapter(this,userNames,images);
+        MyListAdapter mlAdapter = new MyListAdapter(this,friendList,images);
         lv.setAdapter(mlAdapter);
 
         lv.setOnItemClickListener(new AdapterView.OnItemClickListener(){
@@ -40,7 +83,7 @@ public class Messages extends AppCompatActivity {
                 @Override
                 public void onItemClick (AdapterView < ? > adapterView, View view,int i, long l){
 
-                 for (int x=0; x<10 ; x++) {
+                 for (int x=0; x<friendList.length ; x++) {
                     if(x==i){
                     Intent intent = new Intent(Messages.this, MessagingRoom.class);
                     startActivity(intent);
