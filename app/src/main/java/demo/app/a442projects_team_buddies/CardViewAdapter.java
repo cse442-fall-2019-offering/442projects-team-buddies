d1fe4811e23e9b2c3c7d3f263ecd4bf10c722073
vikram <vsingh26@buffalo.edu>
2019-11-18 01:35:28 -0500

    feature to delete courses added

--- app/src/main/java/demo/app/a442projects_team_buddies/CardViewAdapter.java
+++ app/src/main/java/demo/app/a442projects_team_buddies/CardViewAdapter.java
@@ -4,6 +4,7 @@ package demo.app.a442projects_team_buddies;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 import java.util.ArrayList;
@@ -14,17 +15,61 @@ import androidx.recyclerview.widget.RecyclerView;
 public class CardViewAdapter extends RecyclerView.Adapter<CardViewAdapter.ExampleViewHolder>{
     ArrayList<CourseViewItem> courseList;
 
+    private OnItemClickListener mListner;
+
+    // method to handle cardView listener in CourseView
+    public interface OnItemClickListener{
+        void onItemClick(int position);
+        void onDeleteClick(int position);
+    }
+
+    public void setOnItemClickListener(OnItemClickListener listener){
+        mListner= listener;
+    }
+    //---------------------------------------------------
+
     public static class ExampleViewHolder extends RecyclerView.ViewHolder{
             public TextView course;
             public TextView prof;
             public TextView studentCount;
+            public ImageView deleteImage;
 
-            public ExampleViewHolder(@NonNull View itemView) {
+            public ExampleViewHolder(@NonNull View itemView, final OnItemClickListener listener) {
                 super(itemView);
 
                 course = itemView.findViewById(R.id.nameOfClass);
                 prof= itemView.findViewById(R.id.profName);
                 studentCount=itemView.findViewById(R.id.numbOfStudents);
+                deleteImage= itemView.findViewById(R.id.deleteCourse);
+
+                itemView.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        if(listener!= null)
+                        {
+                            int position= getAdapterPosition();
+                            if (position!= RecyclerView.NO_POSITION)
+                            {
+                                listener.onItemClick(position);
+                            }
+                        }
+                    }
+                });
+                deleteImage.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+
+                        if(listener!= null)
+                        {
+                            int position= getAdapterPosition();
+                            if (position!= RecyclerView.NO_POSITION)
+                            {
+                                listener.onDeleteClick(position);
+                            }
+                        }
+                    }
+                });
+
             }
     }
 
@@ -39,7 +84,7 @@ public class CardViewAdapter extends RecyclerView.Adapter<CardViewAdapter.Exampl
     public ExampleViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType)
     {
             View v = LayoutInflater.from(parent.getContext()).inflate(R.layout.cardview_sample, parent,false);
-            ExampleViewHolder evh = new ExampleViewHolder(v);
+            ExampleViewHolder evh = new ExampleViewHolder(v,mListner);
              return evh;
     }
 
